664ca1ed687b10a1b5ba0d3c529b145f3eba0a12
Maksim <bogdanenko1996@gmail.com>
2023-05-12 15:13:35 +0300

    If data is not valid - display error

--- src/main/java/web/controllers/UsersController.java
+++ src/main/java/web/controllers/UsersController.java
@@ -1,14 +1,20 @@
 package web.controllers;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
 import web.models.User;
 import web.services.UserService;
+import org.springframework.validation.ObjectError;
+
+import javax.validation.Valid;
 
 @Controller
 public class UsersController {
@@ -32,7 +38,6 @@ public class UsersController {
 
     @GetMapping("/users")
     public String listOfUsers(Model model) {
-        System.out.println(userService.getAllUsers().size());
         model.addAttribute("users", userService.getAllUsers());
         return "listOfUsers";
     }
@@ -44,14 +49,30 @@ public class UsersController {
     }
 
      @PostMapping("/users")
-    public String createNewUserProfile(@ModelAttribute("user") User user){
-         System.out.println(user.getLastName());
+    public String createNewUserProfile(@Valid User user, BindingResult bindingResult){
+        if(userService.isUserExistsWithEmail(user.getEmail())){
+            bindingResult.rejectValue("email", "error.user", "An account already exists for this email.");
+        }
+
+         for(ObjectError o : bindingResult.getAllErrors()) {
+             System.out.println(o);
+         }
+        if(bindingResult.hasErrors()){
+            return "newUserForm";
+        }
          userService.saveNewUserProfile(user);
         return "redirect:/users";
      }
 
      @PostMapping("users/update/{id}")
-     public String updateUserProfile(@ModelAttribute(name = "user") User user){
+     public String updateUserProfile(@Valid User user, BindingResult bindingResult){
+         if(userService.isUserExistsWithEmail(user.getEmail())){
+             bindingResult.rejectValue("email", "error.user", "An account already exists for this email.");
+         }
+
+         if(bindingResult.hasErrors()){
+             return "editUserForm";
+         }
         userService.updateUserProfile(user);
         return"redirect:/users";
      }
@@ -60,4 +81,7 @@ public class UsersController {
         model.addAttribute("user", userService.getById(id));
         return "editUserForm";
     }
+
+
+
 }
